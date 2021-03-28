9b4fcec883a973c83f2f822a3d9a9e33a8a3aab4
LAPTOP-DI30GGAL\iiisinnnn <watasin.kongkhun@mail.kmutt.ac.th>
2021-03-28 13:59:33 +0700

    คำตอบข้อที่ 3

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tag1 = 0, tag2 = 0, tag3 = 0;
+
+            Console.Write("Input Tank1 : ");
+
+            tag1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Input Tank2 : ");
+
+
+            tag2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Input Tank3 ; ");
+
+            tag3 = int.Parse(Console.ReadLine());
+
+            int A = 0, B = 0;
+
+            //Console.WriteLine("ไม่ไหว จะอ้วกแล้วครับ");
+            
+
+        }
+    }
+}
